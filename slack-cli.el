a0253f538cb43880781078f75d3d9968bbd44ce8
accidentalrebel <accidentalrebel@gmail.com>
2019-07-29 09:46:37 +0800

    Can now reply to buffer by pressing "r".

--- slack-cli.el
+++ slack-cli.el
@@ -1,20 +1,45 @@
-(defun slack-cli-send ()
+(define-derived-mode slack-cli-mode special-mode "slack-cli-mode")
+
+(defun slack-cli-mode--setup-keys ()
+  "Initial config for setting of keys."
+  (local-set-key (kbd "r") 'slack-cli-reply-on-buffer))
+
+(add-hook 'slack-cli-mode-hook 'slack-cli-mode--setup-keys)
+
+(defun slack-cli--refresh-buffer
+    "Refreshes the channel buffer with the latest details"
+    (let (( inhibit-read-only 1))
+      (erase-buffer)
+      (slack-cli-mode)
+      (slack-cli-retrieve channel "10")))
+
+(defun slack-cli-send (&optional _channel)
   "Sends slack message"
   (interactive)
-  (let ((channel (completing-read "Select channel" slack-cli-channels))
-	(message (read-string "Your message: "))
-	)
-    (start-process-shell-command "slack-cli" (concat "*slack-cli:" channel "*") (concat "slack-cli -d " channel " \"" message "\""))))
+  (let* ((channel
+	  (if _channel
+	      _channel
+	    (completing-read "Select channel" slack-cli-channels)))
+	 (message (read-string (concat "Send message to " channel ": ")))
+	 (buffer-name (concat "*slack-cli:" channel "*")))
+    (switch-to-buffer buffer-name)
+    (let ((inhibit-read-only 1))
+      (start-process-shell-command "slack-cli" (concat "*slack-cli:" channel "*") (concat "slack-cli -d " channel " \"" message "\"")))))
 
-(defun slack-cli-retrieve ()
+(defun slack-cli-retrieve (&optional _channel _retrieve-count)
   "Retrieve slack messages"
   (interactive)
-  (let* ((channel (completing-read "Select channel" slack-cli-channels))
-	 (num (read-string "Num to retrieve (default: 10): " nil nil "10"))
+  (let* ((channel (if _channel
+		      _channel
+		    (completing-read "Select channel" slack-cli-channels)))
+	 (num (if _retrieve-count
+		  _retrieve-count
+		(read-string "Num to retrieve (default: 10): " nil nil "10")))
 	 (buffer-name (concat "*slack-cli:" channel "*"))
 	 (retrieved (shell-command-to-string (concat "slack-cli -s " channel " -l " num))))
     (switch-to-buffer buffer-name)
-    (insert retrieved)))
+    (let ((inhibit-read-only 1))
+      (insert retrieved))))
 
 (defun slack-cli-listen ()
   "Listen continuously for slack messages"
@@ -23,10 +48,19 @@
 	 (buffer-name (concat "*slack-cli:" channel "*")))
     (start-process (concat "slack-cli:" channel) buffer-name "slack-cli" "-s" channel)
     (set-process-filter (get-buffer-process buffer-name) 'slack-cli-process-output)
-    (switch-to-buffer buffer-name)))
+    (switch-to-buffer buffer-name)
+    (slack-cli--refresh-buffer)
+    ))
 
 (defun slack-cli-process-output(proc string)
   "Custom process filter to handle specific charaters."
   (goto-char (process-mark proc))
   (insert (ansi-color-apply string))
   (set-marker (process-mark proc) (point)))
+
+(defun slack-cli-reply-on-buffer()
+  "Replies to the slack buffer."
+  (interactive)
+  (let ((channel (car (last (split-string (replace-regexp-in-string "\*" "" (buffer-name)) ":")))))
+    (slack-cli-send channel)))
+
